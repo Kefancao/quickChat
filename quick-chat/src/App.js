bb70276bbc44b74631ed88dfd94c8d80e3e7e02a
Kefan Cao <k33cao@uwaterloo.ca>
2021-08-16 21:23:36 -0400

    Added auth

--- quick-chat/src/App.js
+++ quick-chat/src/App.js
@@ -1,22 +1,36 @@
-import logo from './logo.svg';
+import React from 'react'; 
 import './App.css';
+import './styles.css';
 
+// import firebase from 'firebase/app'; 
+// import 'firebase/firestore'
+// import 'firebase/auth'
+
+// import { useAuthState } from 'react-firebase-hooks/auth'
+// import { useCollectionData } from 'react-firebase-hooks/firestore'
+
+// firebase.initializeApp{
+//   apiKey: "AIzaSyA4fbIfuvkx1DMtZK-Q0jGuzBZruVzVhRM",
+//   authDomain: "quick-chat-db609.firebaseapp.com",
+//   projectId: "quick-chat-db609",
+//   storageBucket: "quick-chat-db609.appspot.com",
+//   messagingSenderId: "557271218039",
+//   appId: "1:557271218039:web:38f82323173721e9111372",
+//   measurementId: "G-B66PQED7GZ"
+// }
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div class="welcome"> 
+          <div class = "title">
+            quickChat
+          </div>
+          <div class="createButton">
+            <a>Join Chat</a>
+            <a>Create Chat</a>
+          </div>
+        </div>
       </header>
     </div>
   );
